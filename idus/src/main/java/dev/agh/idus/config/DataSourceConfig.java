a2f6d7e2c31bc099e1af84dbf59608d657ff52fa
Ahngyuho <gyuho0897@gmail.com>
2025-02-11 11:25:17 +0900

    DB 이중화 설정 코드 작성
    DB 2개(마스터, 슬레이브) 연동하기 위한 설정 클래스 작성

---
+++ idus/src/main/java/dev/agh/idus/config/DataSourceConfig.java
@@ -0,0 +1,71 @@
+package dev.agh.idus.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+
+//이거 안 해주면 못 읽어온다고?
+@Configuration
+public class DataSourceConfig {
+
+    @ConfigurationProperties(prefix = "spring.datasource.master")
+    @Bean
+    public DataSource masterDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @ConfigurationProperties(prefix = "spring.datasource.slave")
+    @Bean
+    public DataSource slaveDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @DependsOn({"masterDataSource", "slaveDataSource"})
+    @Bean
+    public DataSource routingDataSource(
+            @Qualifier("masterDataSource") DataSource master,
+            @Qualifier("slaveDataSource") DataSource slave) {
+        DynamicRoutingDataSource routingDataSource = new DynamicRoutingDataSource();
+
+        Map<Object, Object> dataSourceMap = new HashMap<>();
+
+        dataSourceMap.put("MASTER", master);
+        dataSourceMap.put("SLAVE", slave);
+
+        routingDataSource.setTargetDataSources(dataSourceMap);
+        routingDataSource.setDefaultTargetDataSource(master);
+
+        return routingDataSource;
+    }
+
+    @DependsOn({"routingDataSource"})
+    @Primary
+    @Bean
+    //LazyConnectionDataSourceProxy 는 스프링이 데이터 소스를 만들 때 사용하는 객체!
+    //Primary 를 통해 내가 만든 이 빈을 우선으로 스프링 프레임워크가 사용하도록 하는 것!
+    public DataSource dataSource(DataSource routingDataSource) {
+        return new LazyConnectionDataSourceProxy(routingDataSource);
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(entityManagerFactory);
+        return jpaTransactionManager;
+    }
+}
+
